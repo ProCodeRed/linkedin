dc642bbb33e6ec1ff2171e63e11ea5b35309aa31
ProCodeRed <yadav.vipinkumar@outlook.com>
2021-08-22 02:07:38 +0530

    worked on feed area

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Feed from './Component/Feed';
 import Header from './Component/Header';
 import Sidebar from './Component/Sidebar';
 
--- src/Component/Feed.js
+++ src/Component/Feed.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import './Feed.css'
+import CreateIcon from '@material-ui/icons/Create';
+import ImageIcon from '@material-ui/icons/Image';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
+import InputOption from './InputOption';
 
 function Feed() {
     return (
-        <div>
-            
+        <div className="feed">
+            <div className="feed__inputContainer">
+                <div className="feed__input">
+                    <CreateIcon />
+                    <form>
+                        <input type="text" />
+                        <button type="submit">Send</button>
+                    </form>
+                </div>
+                <div className="feed__inputOptions">
+                    <InputOption title="Photo" Icon={ImageIcon} color="#70B5F9" />
+                    <InputOption title="Video" Icon={SubscriptionsIcon} color="#e7a33e" />
+                    <InputOption title="Event" Icon={EventNoteIcon} color="#c0cbcd" />
+                    <InputOption title="Write article" Icon={CalendarViewDayIcon} color="#70B5F9" />
+                </div>
+            </div>
         </div>
     )
 }
